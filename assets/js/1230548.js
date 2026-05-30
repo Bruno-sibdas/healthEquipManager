b2322ae5794e5cb4145b9d689df0a37d3ec7c4c8
Bruno Costa <1230548@isep.ipp.pt>
2026-05-30 16:31:27 +0100

    Submit com JavaScript

--- assets/js/1230548.js
+++ assets/js/1230548.js
@@ -1,2 +1,15 @@
-// Verifica se o ficheiro JavaScript foi carregado
+// Confirma que o JavaScript foi carregado
 console.log("HealthEquipManager carregado");
+
+// Obtém o formulário através do ID
+const formulario = document.getElementById("formEquipamento");
+
+// Escuta o evento de submissão
+formulario.addEventListener("submit", function(evento) {
+
+    // Impede o comportamento padrão
+    evento.preventDefault();
+
+    console.log("Formulário submetido");
+
+});
