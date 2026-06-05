519e687a4339e5b5c743b18819c7c10ef2563059
Bruno Costa <1230548@isep.ipp.pt>
2026-06-05 21:57:41 +0100

    Tabela fornecedor

--- database/health_equip_manager.sql
+++ database/health_equip_manager.sql
@@ -13,6 +13,7 @@ CREATE TABLE Categoria (
 
 );
 
+
 -- Tabela de localizações físicas dos equipamentos
 CREATE TABLE Localizacao (
 
@@ -25,6 +26,36 @@ CREATE TABLE Localizacao (
     servico VARCHAR(100) NOT NULL,
 
     sala VARCHAR(100)
+
+);
+
+
+-- Tabela de fornecedores
+CREATE TABLE Fornecedor (
+
+    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+
+    nome_empresa VARCHAR(150) NOT NULL,
+
+    nif VARCHAR(20) UNIQUE,
+
+    telefone VARCHAR(20),
+
+    email VARCHAR(150),
+
+    morada VARCHAR(255),
+
+    website VARCHAR(255),
+
+    pessoa_contacto VARCHAR(150),
+
+    telefone_contacto VARCHAR(20),
+
+    tipo_fornecedor VARCHAR(100),
+
+    observacoes TEXT,
+
+    fornecedor_ativo BOOLEAN DEFAULT TRUE
 
 );
 
