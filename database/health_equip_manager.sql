28a425b6cb4831a33b76bcd24ffc6185380dfdac
Bruno Costa <1230548@isep.ipp.pt>
2026-06-05 18:54:09 +0100

    Criação da tabela categoria

--- database/health_equip_manager.sql
+++ database/health_equip_manager.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE IF NOT EXISTS health_equip_manager;
 
 USE health_equip_manager;
+
+-- Tabela de categorias dos equipamentos
+CREATE TABLE Categoria (
+
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+
+    nome VARCHAR(100) NOT NULL,
+
+    descricao VARCHAR(255)
+
+);
+
